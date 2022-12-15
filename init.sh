6f83cd25a734246321554026273412a2b7c78457
AlessioZanga <alessio.zanga@outlook.it>
2022-12-15 19:12:22 +0100

    Fix `winetricks` dependencies

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ Xvfb :5 -screen 0 1024x768x16 &
 WINEDLLOVERRIDES="mscoree=d" wineboot --init /nogui
 winetricks corefonts
 winetricks sound=disabled
-DISPLAY=:5.0 winetricks -q vcrun2019
+DISPLAY=:5.0 winetricks -q vcrun2013
+DISPLAY=:5.0 winetricks -q vcrun2017
 DISPLAY=:5.0 winetricks -q --force dotnet48
